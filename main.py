e3de2a479ba87f9c80066d12f2fdc8ce6336d09c
JimeFioni <jimenafioni@gmail.com>
2023-09-20 00:05:10 -0300

    api

--- main.py
+++ main.py
@@ -26,15 +26,49 @@ sentimiento_analysis = pd.read_parquet("data/sentimiento_analysis.parquet")
 
 
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def incio ():
-    return {"message": "Welcome to the MLOps project"}
+    principal= """
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>API Steam</title>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    padding: 20px;
+                }
+                h1 {
+                    color: #333;
+                    text-align: center;
+                }
+                p {
+                    color: #666;
+                    text-align: center;
+                    font-size: 18px;
+                    margin-top: 20px;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>API de consultas sobre juegos de la plataforma Steam</h1>
+            <p>Bienvenido a la API de Steam donde se pueden hacer diferentes consultas sobre la plataforma de videojuegos.</p>
+            <p>INSTRUCCIONES:</p>
+            <p>Escriba <span style="background-color: lightgray;">/docs</span> a continuación de la URL actual de esta página para interactuar con la API</p>
+            <p>Consulte en el siguiente enlace:<a href="http://127.0.0.1:8000/docs#/">{{FastAPI}}</a></p>
+            <p> El desarrollo de este proyecto esta en <a href="https://github.com/JimeFioni/PI_1-MLOps_Juegos_Steam"><img alt="GitHub" src="https://img.shields.io/badge/GitHub-black?style=flat-square&logo=github"></a></p>
+            <p>María Jimena Fioni - 2023 -</p>
+        </body>
+    </html>
+
+        """    
+    return principal
 
 
 
 
 #Primera función
-@app.get("/userdata/{user_id}", name = "USERDATA")
+@app.get( "/userdata/{user_id}", name = "USERDATA")
 async def userdata(user_id : str):
     """
     La siguiente función retorna información sobre el usuario que se le pasa como argumento
