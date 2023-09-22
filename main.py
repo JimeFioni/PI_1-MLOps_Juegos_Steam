a786ef1c79069c20fcb44d47dc8cda3e4d686d02
JimeFioni <fionijimena@hotmail.com>
2023-09-22 08:27:01 -0300

    http

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ async def incio ():
             <p>Bienvenido a la API de Steam donde se pueden hacer diferentes consultas sobre la plataforma de videojuegos.</p>
             <p>INSTRUCCIONES:</p>
             <p>Escriba <span style="background-color: lightgray;">/docs</span> a continuación de la URL actual de esta página para interactuar con la API</p>
-            <p>Consulte en el siguiente enlace:<a href="http://127.0.0.1:8000/docs#/">{{FastAPI}}</a></p>
+            <p>Consulte en el siguiente enlace:<a href="https://pi1-steamgames-deploy-jimefioni.onrender.com/docs/">{{FastAPI}}</a></p>
             <p> El desarrollo de este proyecto esta en <a href="https://github.com/JimeFioni/PI_1-MLOps_Juegos_Steam"><img alt="GitHub" src="https://img.shields.io/badge/GitHub-black?style=flat-square&logo=github"></a></p>
             <p>María Jimena Fioni - 2023 -</p>
         </body>
